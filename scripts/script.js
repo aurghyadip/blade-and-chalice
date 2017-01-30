7ee816fd67934118dd7489072d1e0c90cddfdf97
Aurghyadip Kundu <adkundu@gmail.com>
2017-01-30 22:12:28 +0530

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,143 @@
+// Author: Aurghyadip Kundu
+// email: - adkundu@gmail.com
+// Done as a part of testing the mojs 
+// library for creating web animations.
+// P.S. - I am also a fan of Dan Brown
+
+const blade = new mojs.Shape ({
+	shape: 'polygon',
+	fill: 'none',
+	radius: 200,
+	stroke: { 'white' : 'black' },
+	strokeWidth: { 5: 0 },
+	strokeDasharray: '100%',
+	strokeDashoffset: { '-100%': '100%' },
+	top: '60%',
+	angle: 0,
+	duration: 5000
+})
+
+const chalice = new mojs.Shape ({
+	shape: 'polygon',
+	fill: 'none',
+	radius: 200,
+	stroke: { 'white' : 'black' },
+	strokeWidth: { 5: 0 },
+	strokeDasharray: '100%',
+	strokeDashoffset: { '-100%': '100%' },
+	top: '60%',
+	angle: 180,
+	duration: 5000
+})
+
+const circ = new mojs.Shape ({
+	shape: 'circle',
+	fill: 'none',
+	radius: 200,
+	stroke: { 'white' : 'black' },
+	strokeWidth: { 5: 0 },
+	top: '60%',
+	strokeDasharray: '100%',
+	strokeDashoffset: { '-100%': '100%' },
+	// angle: 180,
+	duration: 5000
+})
+
+
+
+const burst_blade1 = new mojs.Burst({
+	radius: { 0 : 200 },
+	count: 3,
+	angle: 0,
+	top: '60%',
+	children: {
+		shape: 'polygon',
+		fill: 'none',
+		radius: 50,
+		stroke: { 'white' : 'black' },
+		strokeWidth: { 3: 0 },
+		strokeDasharray: '100%',
+		strokeDashoffset: { '-100%': '100%' },
+		angle: 180,
+		duration: 5000
+	}
+})
+
+const burst_chalice1 = new mojs.Burst({
+	radius: { 0 : 200 },
+	count: 3,
+	angle: 0,
+	top: '60%',
+	children: {
+		shape: 'polygon',
+		fill: 'none',
+		radius: 50,
+		stroke: { 'white' : 'black' },
+		strokeWidth: { 3: 0 },
+		strokeDasharray: '100%',
+		strokeDashoffset: { '-100%': '100%' },
+		angle: 0,
+		duration: 5000
+	}
+})
+
+const burst_blade2 = new mojs.Burst({
+	radius: { 0 : 200 },
+	count: 3,
+	angle: 180,
+	top: '60%',
+	children: {
+		shape: 'polygon',
+		fill: 'none',
+		radius: 50,
+		stroke: { 'white' : 'black' },
+		strokeWidth: { 3: 0 },
+		strokeDasharray: '100%',
+		strokeDashoffset: { '-100%': '100%' },
+		angle: 0,
+		duration: 5000
+	}
+})
+
+const burst_chalice2 = new mojs.Burst({
+	radius: { 0 : 200 },
+	count: 3,
+	angle: 180,
+	top: '60%',
+	children: {
+		shape: 'polygon',
+		fill: 'none',
+		radius: 50,
+		stroke: { 'white' : 'black' },
+		strokeWidth: { 3: 0 },
+		strokeDasharray: '100%',
+		strokeDashoffset: { '-100%': '100%' },
+		angle: 180,
+		duration: 5000
+	}
+})
+
+const burst_circ = new mojs.Burst({
+	radius: { 0 : 200 },
+	count: 6,
+	top: '60%',
+	// angle: 180,
+	children: {
+		shape: 'circle',
+		fill: 'none',
+		radius: 50,
+		stroke: { 'white' : 'black' },
+		strokeWidth: { 3: 0 },
+		strokeDasharray: '100%',
+		strokeDashoffset: { '-100%': '100%' },
+		// angle: 180,
+		duration: 5000
+	}
+})
+
+const timeline = new mojs.Timeline({
+	repeat: 999
+})
+
+.add(blade, chalice, circ, burst_blade1, burst_chalice1, burst_blade2, burst_chalice2, burst_circ)
+.play();
